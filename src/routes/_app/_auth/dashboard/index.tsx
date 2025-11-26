5df3b6f67294dbd63c2965b01a587fca7a9b0f6a
thy-2004 <thy230200@gmail.com>
2025-11-26 18:09:05 +0700

    fix chucnangn

--- src/routes/_app/_auth/dashboard/index.tsx
+++ src/routes/_app/_auth/dashboard/index.tsx
@@ -6,7 +6,6 @@ import { buttonVariants } from "@/ui/button-util";
 import { CreateMenu } from "@/ui/create-menu";
 import { api } from "@cvx/_generated/api";
 import { useQuery } from "convex/react";
-import { Route as AppIdIndexRoute } from "@/routes/_app/_auth/dashboard/apps/$appId/index";
 
 export const Route = createFileRoute("/_app/_auth/dashboard/")({
   component: Dashboard,
